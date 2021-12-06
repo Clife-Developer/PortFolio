fd07b3acd8ca810abd97c959bfbe379da208561d
Clife-Developer <clife.united@gmail.com>
2021-12-06 16:03:19 +0200

    init commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,60 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar navbar-expand-lg navbar-light bg-primary">
+       <div className="container-fluid">
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+         <div className="navbar-nav">
+           <a href="#" class="nav-link "><b>HOME</b></a>
+          </div>
+         </div>
+        </div>
+       </nav>
+       
+      <div className="container-fluid row justify-content-center">
+        <div className="col-sm-7">
+          <div classNamw="card  justify-content-center">
+          <div className="card-body">
+           <div className="img"></div>
+            <h1 className="Names">CLIFE MHLONGO</h1>
+            <hr className=""/>
+            <br/>
+            <p className="card-text">
+             I am a motivated, confident, and results driven computer science graduate from Lobachevksy State University. 
+             I am currently a junior self-taught sofware developer with no professional work experience. I have the ability
+             to efficiently prioritize and multitask projects. I always do my best to
+             achieve the highest level of quality and efficiency. I enjoy working with other people, especially
+             as a team and I am also comfortable working on my own. I also enjoy
+             helping other people with their tasks or projects.
+            </p>
+            <br/>
+            <p>
+              Currently I am a junior level full stack developer with an emphasis on: JavaScript, 
+              Angular10+, React.js, Java (programming language), MongoDB, node.js, mongoose, express.js, 
+              redux, MongoDB, MySQL, SQL, postman, firebase, REST API, JSON, AJAX, bcrypt, JWT, dotenv, CSS,
+              Bootstrap, HTML
+              As a frontend developer, I am quite comfortable with the latest trends and standards in Frontend 
+              development. I am a fast learner and a team-player. You may count on me being an asset to your project. 
+              If the project requires the knowledge of other frameworks, currently not part of my stack, I am willing 
+              to quickly learn them.
+              As a backend developer, I am quite comfortable with the latest trends and standards in backend 
+              development. I am always striving to increase my knowledge and keep myself updated and to keep my 
+              skills relevant in the world of software development. I am willing to quickly learn any technology 
+              that may be required for any project.
+
+            </p>
+            <br/>
+            <p>
+             I would like to work for SovTech, based on your history you seem
+             to be a very focused company with big plans for the future, I see myself advancing and growing professionally with your company.
+             I am very much interested in gaining hands-on knowledge because it will ultimately complement my 
+             theoretical knowledge and help me improve in my skills, I believe my skillset can
+             add value to your company
+            </p>
+           </div>
+        </div>
+        </div>
+      </div>
     </div>
   );
 }
